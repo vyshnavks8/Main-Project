3cd628b65da140716a8cc13b764a685575456d95
Vyshnav KS <vyshnavks8@gmail.com>
2020-03-15 18:37:23 +0530

    Update ShortPathSelector.cs

--- Assets/Scripts/ShortPathSelector.cs
+++ Assets/Scripts/ShortPathSelector.cs
@@ -6,25 +6,62 @@ using UnityEngine.AI;
 public class ShortPathSelector : MonoBehaviour
 {
     public NavMeshAgent agent;
-    private LineRenderer line;
     public Transform destination;
     Vector3 point;
+    Vector3 tempPoint;
     NavMeshPath path;
+    bool temp = false;
+    HashSet<string> set;
     void Start()
     {
-        line = GetComponent<LineRenderer>();
-        line.startWidth = 10;
-        line.endWidth = 10;
+         set= new HashSet<string>();
     }
     void Update()
     {
         point = new Vector3(destination.position.x, 0, destination.position.z);
         path = new NavMeshPath();
         agent.CalculatePath(point, path);
-        if (path.status == NavMeshPathStatus.PathComplete)
+        if (temp == false)
         {
-            line.positionCount = path.corners.Length;
-            line.SetPositions(path.corners);
+            if (path.status == NavMeshPathStatus.PathComplete)
+            {
+                tempPoint = point;
+                Ray(path); 
+            }
+            temp = true;
         }
+        else if(tempPoint!=point)
+        {
+
+            temp = false;
+            set.Clear();
+        }
+        
+    }
+    void Ray(NavMeshPath path)
+    {
+        LayerMask mask = LayerMask.GetMask("Places");
+        RaycastHit hit;
+        for (int i = 0; i < path.corners.Length; i++)
+        {
+            Vector3 dir = (path.corners[i + 1] - path.corners[i]);
+            if (i == path.corners.Length - 2)
+            {
+                break;
+            }    
+            else if (Physics.Raycast(path.corners[i], dir, out hit, Mathf.Infinity, mask))
+            {  
+                string a = hit.collider.name;
+                set.Add(a);  
+                Debug.Log("city"+a);
+            }
+            else
+            {
+                //not hit
+            }
+        }
+        foreach (string ob in set)
+            Debug.Log("list element" + ob);
+
     }
 }
